5a0f4d0db0ecfc473af74aa5da322a38e991ff97
Riju-bak <defaltsith@gmail.com>
2022-05-08 11:50:38 -0400

    init db connect and modularize

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
 const express = require('express');
 const app = express();
+const Note = require('./models/note');
+require('dotenv').config()
 
 app.use(express.json()); //Without this req.body for POST will be undefined.
 
-const cors = require('cors')
+const cors = require('cors');
 
-app.use(cors())
-app.use(express.static('build'))
-
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        date: "2022-05-30T17:30:31.098Z",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2022-05-30T18:39:34.091Z",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2022-05-30T19:20:14.298Z",
-        important: true
-    },
-    {
-        id: 4,
-        content: "React useEffect runs the side-effect code using handler after every render.",
-        date: "2022-05-30T19:20:14.298Z",
-        important: false
-    }
-]
+app.use(cors());
+app.use(express.static('build'));  //whenever express gets an HTTP GET request it will first check if the build directory contains a file corresponding to the request's address.
+// Now HTTP GET requests to the address www.serversaddress.com/index.html or www.serversaddress.com will show the React frontend.
+// GET requests to the address www.serversaddress.com/api/notes will be handled by the backend's code.
 
 const generateID = () => {
     return notes.length > 0 ? Math.max(...notes.map(note => note.id)) + 1 : 0;
@@ -44,33 +21,37 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/notes', (req, res) => {
-    res.send(notes);
-})
+    Note.find({}).then(notes => {
+        res.send(notes)
+    })
+});
 
 app.get('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const note = notes.find(note => note.id === id);
-    if(note){
+    const id = req.params.id
+    Note.find({_id: id}).then(
+        note => {
+            res.json(note);
+        }
+    )
+    if (note) {
         res.json(note);
-    }
-    else{
+    } else {
         res.status(404).end(`Note with id ${id} doesn't exist`);
     }
 })
 
 app.delete('/api/notes/:id', (req, res) => {
     const id = Number(req.params.id);
-    if(notes.some(note => note.id === id)){
-        notes = notes.filter(note => note.id!==id);
+    if (notes.some(note => note.id === id)) {
+        notes = notes.filter(note => note.id !== id);
         res.status(204).end();
-    }
-    else{
+    } else {
         res.status(404).end();
     }
 })
 app.post('/api/notes', (req, res) => {
     const body = req.body;
-    if(!body.content){
+    if (!body.content) {
         return res.status(400).json({
             error: 'content missing'
         });
@@ -87,7 +68,7 @@ app.post('/api/notes', (req, res) => {
     res.json(note);
 })
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 })
---
+++ models/note.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const url = process.env.MONGODB_URI;
+console.log(`Connecting to MongoDB`);
+
+mongoose.connect(url)
+    .then(res => console.log(`Connected to MongoDB`))
+    .catch(err => console.log(`error connecting to MongoDB: ${err}`));
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    date: Date,
+    important: Boolean
+});
+
+noteSchema.set('toJSON', {
+    transform: (doc, ret) => {
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.__v;
+    }
+});
+
+module.exports = mongoose.model('Note', noteSchema);
+
